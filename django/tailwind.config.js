5ff050f722366a64d54738529e8184eab5ea8582
marc406 <m.ebenwaldner@icloud.com>
2024-06-04 15:14:41 +0200

    added tailwind integration

---
+++ django/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './cisco_configurator_app/templates/configurations/',
+    './cisco_configurator_app/templates/index.html',
+    // Add paths to other apps if necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
